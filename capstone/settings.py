3bcf523a0c760c0706616abb0d9d63d408977031
Josh Pulley <josh.pulley@icloud.com>
2021-12-06 15:15:26 -0600

    url change

--- capstone/settings.py
+++ capstone/settings.py
@@ -59,7 +59,8 @@ REST_FRAMEWORK = {
 }
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://sns-client-capstone.herokuapp.com'
 )
 
 MIDDLEWARE = [
